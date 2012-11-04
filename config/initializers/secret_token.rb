326cb5ebae5788a14b4f707986461adc567ad92d
Gernot Gutschi <gg@bitpro.at>
2012-11-04 12:08:37 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InvoiceManager::Application.config.secret_token = '4bfce9792ad7d605c1454878ad094785061559077ea7bfab3afec72fcd8dda8e65e17ecc91e37b2758ba13024ccbf527b9fc5ff910fe63b27269707b91908c1b'
